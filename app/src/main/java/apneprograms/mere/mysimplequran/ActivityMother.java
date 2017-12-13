5375c6e434c948eaf5c9d93c07529a554007545e
Student <phone.asim@gmail.com>
2017-12-13 19:55:37 +1100

    Starting 1st

---
+++ app/src/main/java/apneprograms/mere/mysimplequran/ActivityMother.java
@@ -0,0 +1,13 @@
+package apneprograms.mere.mysimplequran;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityMother extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mother);
+    }
+}
